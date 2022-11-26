8124772f2642e0e5aa11c959c467e6fc617495ac
Ik <117489623+DaanSchaap@users.noreply.github.com>
2022-11-26 21:53:25 +0100

    Fixes

---
+++ src/js/layout/navigation.js
@@ -0,0 +1,45 @@
+// ------------ //
+// Nav Settings //
+// ------------ //
+
+let navSettings = [
+    {
+        label: "Home",
+        href: "/",
+        pageTitle: "Home",
+        pageDescription: "Website Description",
+        type: "link"
+    },
+    {
+        label: "About",
+        href: "/About.html",
+        pageTitle: "About",
+        pageDescription: "About Website",
+        type: "link"
+    },
+    {
+        label: "Contact",
+        href: "/Contact.html",
+        pageTitle: "Contact",
+        pageDescription: "Contact.............",
+        type: "link"
+    }
+];
+
+// -------- //
+// Nav Code //
+// -------- //
+
+document.body.insertAdjacentHTML(`afterbegin`, `<nav class="primary-navigation"><ul class="primary-navigation-items"></ul></nav>`);
+
+navSettings.forEach(function (index, value) {
+    if (index.type === "link") {
+        document.querySelector(`.primary-navigation-items`).insertAdjacentHTML(`beforeend`, `<li class="primary-navigation-item page-` + index.label.toLowerCase() + ` color-page-` + index.label.toLowerCase() + `"><a href="` + index.href + `">` + index.label + `</a></li>`);
+
+        if (index.href === location.pathname) {
+            document.querySelector(`.page-` + index.label.toLowerCase()).classList.add(`primary-navigation-nav-active`);
+
+            document.querySelector(`:root`).style.setProperty(`--color`, `var(--page-` + index.label.toLowerCase() + `, var(--accent-primary))`)
+        }
+    }
+});
